f335089f4ef5aa1b3822aa414d36df71a392e380
Dongseong <hpdh12@me.com>
2023-07-05 15:53:15 +0900

    [refactor] generalize whole case

--- PrimeFactorsKata/prime-factors.cpp
+++ PrimeFactorsKata/prime-factors.cpp
@@ -9,25 +9,14 @@ public:
 	{
 		vector<int> result = {};
 
-		if (num > 1)
-		{
-			int divisor = 2;
-			if(num == 4 || num == 6 || num == 9 || num == 12)
+		for (int divisor = 2; num > 1; divisor++) {
+			while ((num % divisor) == 0)
 			{
-				for (divisor = 2; num > 1; divisor++) {
-					while ((num % divisor) == 0)
-					{
-						result.push_back(divisor);
-						num /= divisor;
-					}
-				}
-			}
-			else
-			{
-				result.push_back(num);
+				result.push_back(divisor);
+				num /= divisor;
 			}
 		}
-
+		
 		return result;
 	}
 };
