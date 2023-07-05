e40a644f0464f9bbf08d4e4e979023e0e16412de
Dongseong <hpdh12@me.com>
2023-07-05 15:39:17 +0900

    [refactor] replace 2 to variable

--- PrimeFactorsKata/prime-factors.cpp
+++ PrimeFactorsKata/prime-factors.cpp
@@ -11,20 +11,21 @@ public:
 
 		if (num > 1)
 		{
+			int divisor = 2;
 			if (num == 4)
 			{
-				while ((num % 2) == 0)
+				while ((num % divisor) == 0)
 				{
-					result.push_back(2);
-					num /= 2;
+					result.push_back(divisor);
+					num /= divisor;
 				}
 			}
 			else if(num == 6)
 			{
-				while((num % 2) == 0)
+				while((num % divisor) == 0)
 				{
-					result.push_back(2);
-					num /= 2;
+					result.push_back(divisor);
+					num /= divisor;
 				}
 				while ((num % 3) == 0)
 				{
